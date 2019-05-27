05e89dcdc5684e5e3b9e6f4e714f90bfe76da3fb
github username <github email address>
2019-05-27 17:25:41 -0400

    done usung sort_by

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,10 +4,11 @@ def alphabetize(arr)
   num_arr=[]
   array_espe=ESPERANTO_ALPHABET.split("")
   arr.sort_by do |word|
+    num_arr=[]
     splitted_word=word.split("")
-    splitted_word.map do |char|
-       array_espe.index(char)
-    end
-    
+      splitted_word.each do |char|
+        num_arr << array_espe.index(char)
+      end
+    num_arr
   end
 end
