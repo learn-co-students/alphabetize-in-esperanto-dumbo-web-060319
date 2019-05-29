fd336e83f0beae05d5ba51b0535b3a73f88b4d6a
Alexander Schelchere <ascellie@gmail.com>
2019-05-29 08:29:12 -0400

    Trying to use insertion sort - This gets everything but the first character

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,22 @@
+require "pry"
+
 def alphabetize(arr)
-  # code here
+  sorted = []
+  sorted.push(arr.shift)
+  # binding.pry
+  for i in arr
+    sorted_index = 0
+    while sorted_index < sorted.length
+      if i <= sorted[sorted_index]
+        sorted.insert(sorted_index, i)
+        break
+      elsif sorted_index == sorted.length - 1
+        sorted.insert(sorted_index + 1, i)
+        break
+      end
+      sorted_index += 1
+    end
+  end
+  sorted
 end
+
